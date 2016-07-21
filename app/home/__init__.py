c3129d5d4b4107aada3e3d7d7ce4ebd2dec6a6cc
Hitesh Garg <garg95hitesh@gmail.com>
2016-07-21 22:34:49 +0530

    search by organisation(sorted by name) working

--- app/home/__init__.py
+++ app/home/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 
+
 home = Blueprint('home', __name__)
 
 from . import routes
--- app/home/routes.py
+++ app/home/routes.py
@@ -1,6 +1,22 @@
-from flask import render_template
+from flask import render_template, request
 from . import home
+import requests
 
-@home.route('/')
+
+@home.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        org_name = request.form['search']
+        #main code starts here!
+        #org_name = str(raw_input("Enter the name of the organisation: ")).lower()
+        api_url = "https://api.github.com/orgs/"+ org_name + "/repos"
+        r= requests.get(api_url)
+        json_obj = r.json()
+        try:
+            json_obj['message']
+            context_dict = {"message":"No such organisation exists!"}
+            return render_template('home/results.html',context_dict=context_dict)
+        except:
+            sorted_list = sorted(json_obj, key=lambda k: k['name'], reverse = False)
+            return render_template('home/results.html',org_name=org_name,sorted_list=sorted_list)
     return render_template('home/index.html')
