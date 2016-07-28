c5267356d9f5cf2314723a4db509cba948394ae4
HItesh Garg <garg95hitesh@gmail.com>
2016-07-28 22:14:36 +0530

    Redirection from '/home' to '/'

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
 
     from .home import home as home_blueprint
-    app.register_blueprint(home_blueprint,url_prefix='/home')
+    app.register_blueprint(home_blueprint)
     from .orgs import orgs as orgs_blueprint
     app.register_blueprint(orgs_blueprint,url_prefix='/orgs')
     from .indv import indv as indv_blueprint
