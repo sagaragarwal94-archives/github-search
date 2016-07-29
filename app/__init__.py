2ac15a8ccada01f5a676b49d2f1a469f4ca96f95
Sagar Chand Agarwal <atomsagar@gmail.com>
2016-07-28 22:03:20 -0700

    heroku error solved

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config.from_object(config[config_name])
+    #app.config.from_object(config[config_name])
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
