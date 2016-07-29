277658113e606d897f1fe2a7df8286cc07a51e4c
Sagar Chand Agarwal <atomsagar@gmail.com>
2016-07-29 03:12:02 -0700

    Template Testing 1

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,10 @@
 from flask import Flask
-#from config import config
 
 def create_app(config_name):
     app = Flask(__name__)
-    #app.config.from_object(config[config_name])
 
-    from .home import home as home_blueprint
-    app.register_blueprint(home_blueprint)
     from .orgs import orgs as orgs_blueprint
-    app.register_blueprint(orgs_blueprint,url_prefix='/orgs')
+    app.register_blueprint(orgs_blueprint)
     from .indv import indv as indv_blueprint
     app.register_blueprint(indv_blueprint,url_prefix='/indv')
 
