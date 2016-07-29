ce306d980f815574b438e60d89af52e0a14782ef
HItesh Garg <garg95hitesh@gmail.com>
2016-07-29 22:03:04 +0530

    Individual all sort added (temporary)

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from config import config
+#from config import config
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config.from_object(config[config_name])
+    #app.config.from_object(config[config_name])
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
